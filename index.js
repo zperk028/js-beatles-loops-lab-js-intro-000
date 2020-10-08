cf9f41bd77c8cc5edf59a1bcfba46ac1a7b5aca6
Zachary Perkins <ezmac028@gmail.com>
2020-10-08 16:52:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,15 +8,8 @@ function theBeatlesPlay (musicians, instruments) {
   return array
 }
 
-function johnLennonFacts () {
-  var facts = [
-    "He was the last Beatle to learn to drive",
-    "He was never a vegetarian",
-    "He was a choir boy and boy scout",
-    "He hated the sound of his own voice",
-    "While on tour, he enjoyed playing Monopoly",
-    "Harmonica was the first instrument Lennon learned to play"
-  ];
+function johnLennonFacts() {
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
   var array = [];
   var i = 0;
   while (array.length < facts.length) {
