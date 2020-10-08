64c3e7d8edc8a7460fe7c814ca42ecbfba3e3f3b
Zachary Perkins <ezmac028@gmail.com>
2020-10-08 16:55:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+
+function theBeatlesPlay (musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < musicians.length; i++) {
+    array.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return array
+}
+
+function johnLennonFacts(facts){
+  let finalArray = [];
+
+  for(let i=0; i<facts.length; ++i){
+    finalArray.push(facts[i]+"!!!");
+  }
+
+  return finalArray;
+}
+
+function iLoveTheBeatles(number){
+  let finalArray = [];
+
+  do{
+    finalArray.push("I love the Beatles!");
+    number++;
+  } while(number < 15)
+  return finalArray;
+}
